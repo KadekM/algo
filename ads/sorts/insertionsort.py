3cc5127c6045d8e7be37762979cca7e7b403d8ab
Marek Kadek <kadek.marek@gmail.com>
2016-03-04 21:37:07 +1300

    insertion sort

---
+++ ads/sorts/insertionsort.py
@@ -0,0 +1,18 @@
+# O(n^2)
+def insertion_sort(xs):
+    n = len(xs)
+    for i, x in enumerate(xs):
+        j = i-1
+        while j >= 0 and xs[j] > x:
+            xs[j+1] = xs[j]
+            j -= 1
+        xs[j+1] = x
+
+
+
+import random
+arr = random.sample(range(20), 15)
+
+print arr
+insertion_sort(arr)
+print arr
