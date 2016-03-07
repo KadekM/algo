0e661eb4cfcf8082993330733bf6df3185725750
Marek Kadek <kadek.marek@gmail.com>
2016-03-07 16:11:59 +1300

    qsort notes

--- ads/sorts/qsort.py
+++ ads/sorts/qsort.py
@@ -1,9 +1,16 @@
+# Not in memory. Only conceptual impl
+# Quick sort is by definition in memory sort!
+
 def qsort_rec(xs):
     if len(xs) <= 1: return xs
     pivot = 0
     left = list(filter(lambda x: x < xs[pivot], xs[1:]))
     right = list(filter(lambda x: x >= xs[pivot], xs[1:]))
     return qsort_rec(left) + [xs[pivot]] + qsort_rec(right)
+
+
+# Correct qsort
+def qsort(xs): pass
 
 
 import random
