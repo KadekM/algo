5a23bf5f4f96506bfee240dab652cbbe444530a6
Marek Kadek <kadek.marek@gmail.com>
2016-03-09 20:03:10 +1300

    heap

---
+++ ads/heap/binary_max_heap.py
@@ -0,0 +1,46 @@
+def find_max(xs): return xs[0]
+
+def delete_max(xs):
+    max = xs[0]
+    xs[0] = xs.pop(len(xs)-1) # root is now last element, last element is removed
+    shift_down(xs, 0)
+    return max
+
+def heap_swap(xs, i, j):
+    tmp = xs[i]
+    xs[i] = xs[j]
+    xs[j] = tmp
+
+def shift_up(xs, i):
+    if i <= 0: return
+
+    parent = (i-1)//2
+    if xs[i] > xs[parent]:
+        heap_swap(xs, i, parent)
+        shift_up(xs, parent)
+
+
+def shift_down(xs, i):
+    child = i * 2 + 1
+
+    if child >= len(xs): return
+
+    if child + 1 < len(xs) and xs[child] < xs[child+1]:
+        child += 1
+
+    if xs[child] > xs[i]:
+        heap_swap(xs, i, child)
+        shift_down(xs, child)
+
+
+def insert(xs, x):
+    xs.append(x)
+    shift_up(xs, len(xs)-1)
+
+
+def increase_key(xs, i, dx):
+    xs[i] += dx
+    if dx > 0: shift_up(xs, i)
+    elif dx < 0: shift_down(xs, i)
+
+def merge(): pass
---
+++ ads/heap/binary_max_heap.test.py
@@ -0,0 +1,20 @@
+from ads.heap.binary_max_heap import *
+
+hp = []
+insert(hp, 5)
+insert(hp, 7)
+insert(hp, 10)
+print(hp)
+
+insert(hp, 4)
+insert(hp, 8)
+insert(hp, 12)
+print(hp)
+
+print(delete_max(hp))
+print(delete_max(hp))
+print(hp)
+increase_key(hp, 2, 5)
+print(hp)
+increase_key(hp, 0, -6)
+print(hp)
